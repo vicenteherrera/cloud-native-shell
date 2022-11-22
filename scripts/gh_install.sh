97ad9002f7fa44029a7b3f2951b3792238b4fd51
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2022-11-22 20:25:23 +0100

    feat: more heuristic and better error handling for gh install script

--- scripts/gh_install.sh
+++ scripts/gh_install.sh
@@ -8,11 +8,14 @@ set -euo pipefail
 # repository as username/reponame in GitHub URL
 [ "$REPO" == "" ] && echo "REPO env variable required" && exit 1
 # filename to download from releseas channel, include VERSION when it is part of the name
+default_zfile="vVERSION.tar.gz"
 : "${ZFILE:=vVERSION.tar.gz}"
 # File or directory you want to move to /usr/local/bin if downloaded is a compressed file 
 : "${FILE:=$ZFILE}"
 # Name of the file or directory after moving to /usr/local/bin if different
 : "${XFILE:=$FILE}"
+
+echo "# Install GH $REPO"
 
 GHTOKEN="${GHTOKEN:-}"
 ghtoken_param=""
@@ -21,19 +24,19 @@ if [ "$GHTOKEN" != "" ]; then
   echo "Using GitHub API token"
 fi
 
-url="https://api.github.com/repos/${REPO}/releases/latest"
-http_code=$(curl --silent -L $ghtoken_param --max-time 30 --output version.txt --write-out "%{http_code}" "$url" ||:)
+url1="https://api.github.com/repos/${REPO}/releases/latest"
+http_code=$(curl --silent -L $ghtoken_param --max-time 30 --output version.txt --write-out "%{http_code}" "$url1" ||:)
 if [[ ${http_code} -eq 403 ]] ; then
-  echo "Error 403: Forbidden"
+  echo "**Error 403: Forbidden"
   echo "GitHub API may have rate limited your IP address. Please wait and retry build."
   if [ "$GHTOKEN" == "" ]; then
     echo "You should use a GitHub API token to avoid rate limiting"
   fi
-  echo $url
+  echo $url1
   exit 1
 elif [[ ${http_code} -lt 200 || ${http_code} -gt 299 ]] ; then
-  echo "Error $http_code getting version"
-  echo "Check URL is correct: $url"
+  echo "**Error $http_code getting version"
+  echo "Check URL is correct: $url1"
   exit 1
 fi
 version=$(cat version.txt | jq ".tag_name" | xargs )
@@ -45,28 +48,57 @@ if [ "${version:0:1}" == "v" ]; then
 fi
 
 # Guess all filenames to use
-if [ "$ZFILE" == "vVERSION.tar.gz" ] && [ "$FILE" == "$ZFILE" ] && [ "$XFILE" == "$ZFILE" ]; then
+if [ "$ZFILE" == "$default_zfile" ] && [ "$FILE" == "$ZFILE" ] && [ "$XFILE" == "$ZFILE" ]; then
   echo "Applying heuristics to get url and file names"
   FILE=$(echo "$REPO" | sed 's|[a-zA-Z0-9_-]*/||')
   XFILE="$FILE"
-  separator="(_|-|)"
-  osName="linux"
-  osArch="(x86_64|amd64|64bit)"
+  sep="(_|-|)"
+  osName="(L|l)inux"
+  osArch="(x86_64|amd64|64bit|)"
   extension="(zip|tar\.gz|deb)"
-  echo "Running query from: curl -sSfL https://api.github.com/repos/${REPO}/releases/latest"
-  url=$(curl -sSfL "https://api.github.com/repos/${REPO}/releases/latest" | grep -Eio "browser_download_url.*${separator}${osName}${separator}${osArch}\.${extension}")
+  optVersion="(v)?(\.[0-9])*"
+  echo "Running query from: https://api.github.com/repos/${REPO}/releases/latest"
+  url1="https://api.github.com/repos/${REPO}/releases/latest"
+  http_code=$(curl --silent -L $ghtoken_param --max-time 30 --output content.txt --write-out "%{http_code}" "$url1" ||:)
+  if [[ ${http_code} -eq 403 ]] ; then
+    echo "**Error 403: Forbidden"
+    echo "GitHub API may have rate limited your IP address. Please wait and retry build."
+    if [ "$GHTOKEN" == "" ]; then
+      echo "You should use a GitHub API token to avoid rate limiting"
+    fi
+    echo $url1
+    exit 1
+  elif [[ ${http_code} -lt 200 || ${http_code} -gt 299 ]] ; then
+    echo "**Error $http_code getting version"
+    echo "Check URL is correct: $url1"
+    exit 1
+  fi
+  url=$(cat content.txt | grep -Eio "browser_download_url.*${sep}${optVersion}${sep}${osName}${sep}${osArch}${sep}${optVersion}"'"' | head -n1 ||:)
+  urlne=url
+  if [ "$url" == "" ]; then
+    echo "No file without extension for download"
+    url=$(cat content.txt | grep -Eio "browser_download_url.*${sep}${optVersion}${sep}${osName}${sep}${osArch}${sep}${optVersion}\.${extension}" | head -n1 ||:)
+  else  
+    echo "Found file without extension for download"
+    FILE=""
+  fi
   if [ "$url" == "" ]; then
     echo "**Error, couldn't guess url or file to download"
+    cat content.txt | grep -Eio "browser_download_url.*"
     exit 1
   elif (( $(grep -c . <<<"$url") > 1 )); then
     echo "More than one possible file to download:"
     echo $url
     url="${url##*$'\n'}"
     echo "Using: $url"
+    exit 1
   fi
+  rm content.txt
   url=${url//\"}
   url=${url/browser_download_url: /}
+  echo "At $url"
   ZFILE=$(basename "$url")
+  [ "$FILE" == "" ] && FILE="$ZFILE"
 fi
 
 # Download from automatic source code attached to tag
@@ -85,7 +117,7 @@ if [ "$url" == "" ]; then
   url="https://github.com/${REPO}/releases/download/${usev}${version}/${ZFILE}"
 fi
 
-echo "$XFILE $version (https://github.com/$REPO)" | tee -a sbom.txt
+echo "$XFILE $version (https://github.com/$REPO)"
 echo "URL: $url"
 echo "Download: $ZFILE, Extract: $FILE, Install as: $XFILE"
 
@@ -104,35 +136,33 @@ echo "File downloaded"
 [ ! -s "${ZFILE}" ] && echo "Error: file is empty, please wait and retry building to download again" && exit 1
 
 extension="${ZFILE##*.}"
-if [[ "${ZFILE}" == "${FILE}" || "${ZFILE}" != *"."* ]]; then
-  echo "File is not compressed"
-  sudo chmod a+x ${ZFILE}
-  sudo mv ${ZFILE} /usr/local/bin/${XFILE}
-  echo "Installed as /usr/local/bin/${XFILE}"
-elif [ "$extension" == "deb" ]; then
+if [ "$extension" == "deb" ]; then
   echo "File is a deb package"
   sudo apt-get install ./"${ZFILE}"
   rm "${ZFILE}"
   echo "Installed deb package ${ZFILE}"
-elif [ "$extension" == "zip" ]; then
-  echo "File is a zip"
-  unzip "${ZFILE}" ${FILE} -d ./
-  rm "${ZFILE}"
+  echo "$ZFILE $version (https://github.com/$REPO)" | tee -a sbom.txt
+else 
+  if [[ "${ZFILE}" == "${FILE}" ]]; then
+    echo "File is not compressed"    
+  elif [ "$extension" == "zip" ]; then
+    echo "File is a zip"
+    unzip "${ZFILE}" ${FILE} -d ./
+    rm "${ZFILE}"
+  elif [ "$extension" == "gz" ] || [ "$extension" == "tgz" ]; then
+    echo "File is gzipped"
+    tar -xvzf "${ZFILE}" ${FILE}
+    rm "${ZFILE}"
+  else
+    echo "Unknown file type: $extension"
+    exit 1
+  fi
   sudo chmod a+x ${FILE}
   sudo chmod -R a+rX ${FILE}
   sudo mv ${FILE} /usr/local/bin/${XFILE}
   echo "Installed as /usr/local/bin/${XFILE}"
-elif [ "$extension" == "gz" ] || [ "$extension" == "tgz" ]; then
-  echo "File is gzipped"
-  tar -xvzf "${ZFILE}" ${FILE}
-  rm "${ZFILE}"
-  sudo chmod a+x ${FILE}
-  sudo chmod -R a+rX ${FILE}
-  sudo mv ${FILE} /usr/local/bin/${XFILE}
-  echo "Installed as /usr/local/bin/${XFILE}"
-else
-  echo "Unknown file type: $extension"
-  exit 1
+  size=$(du --apparent-size --block-size=1 -h "/usr/local/bin/${XFILE}" | awk '{ print $1 }')
+  echo "$XFILE $version (https://github.com/$REPO) $size" | tee -a sbom.txt
 fi
 
 cd ..
